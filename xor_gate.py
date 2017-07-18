b78f07890b849d361416a1bdfabe0214ee61ff86
kenta-s <knt01222@gmail.com>
2017-07-18 09:30:49 +0900

    Create XOR gate

---
+++ xor_gate.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+Y = np.array([[0], [1], [1], [0]])
+
+x = tf.placeholder(tf.float32, shape=[None, 2])
+y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.truncated_normal([2, 2]))
+b = tf.Variable(tf.zeros([2]))
+h = tf.nn.sigmoid(tf.matmul(x, W) + b)
+
+V = tf.Variable(tf.truncated_normal([2, 1]))
+c = tf.Variable(tf.zeros([1]))
+y = tf.nn.sigmoid(tf.matmul(h, V) + c)
